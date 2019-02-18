fa0f18b6690574278116a36dc1060cf084f10a3b
jirizikes <zikes@collabim.com>
2019-02-18 19:34:57 +0100

    Data downloader

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
+import requests
+import json
 
-print("Hello world from python")
+#DEFAULT VARIABLES
+USERTECH_API_URL = "http://carvago-temporary.utdigit.com:8000/api/cars?limit=1000&offset="
+DEFAULT_OFFSET = 0
+
+#DOWNLOADING PROCESS
+print("Starting downloading process...")
+
+current_offset = DEFAULT_OFFSET
+
+while 1 == 1:
+    url = USERTECH_API_URL + str(current_offset)
+
+    response = requests.get(url)
+    response = json.loads(response.text)
+
+    print("OK")
+
+    last = 0
+
+    for ad in response:
+        if ad == "error":
+            last = 1
+        break
+
+    if last:
+        break
+
+    current_offset += 1000
+
+print("Job done!")
